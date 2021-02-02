1bc920e82254fa416a894589835ca17495402898
joed <joed.ngangmeni@bison.howard.edu>
2021-02-01 23:36:03 -0500

    working prework

---
+++ Prework With Storyboard/Prework With StoryboardTests/Prework_With_StoryboardTests.swift
@@ -0,0 +1,33 @@
+//
+//  Prework_With_StoryboardTests.swift
+//  Prework With StoryboardTests
+//
+//  Created by Joëd on 2/1/21.
+//
+
+import XCTest
+@testable import Prework_With_Storyboard
+
+class Prework_With_StoryboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
